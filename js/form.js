d066cfadc0d41bf6d53167d0ef42faad9f2490ed
Leon Santana <leon.santana854@gmail.com>
2025-03-25 05:06:18 -0300

    Finalizando contatos

--- js/form.js
+++ js/form.js
@@ -1,34 +1,43 @@
-
 //class contato
-
 class contato {
-    constructor(nome, email, telefone, tipoContato, mensagem) {
+    constructor(nome, sobrenome, email, cpf, telefone, tipoContato) {
         this.nome = nome;
+        this.sobrenome = sobrenome;
         this.email = email;
+        this.cpf = cpf;
         this.telefone = telefone;
         this.tipoContato = tipoContato;
-        this.mensagem = mensagem;
     }
-
 }
 
 function Post(form) {
 
-    let data = new contato(form.elements.namedItem("nome").value,
+    let tipoContato = form.elements.namedItem("contato").value;
+
+
+    if (tipoContato !== "TELEFONE" && tipoContato !== "E-MAIL") {
+        alert('Por favor, selecione como deseja ser contatado: TELEFONE ou E-MAIL.');
+        return false;
+    }
+    let data = new contato(
+        form.elements.namedItem("nome").value,
         form.elements.namedItem("sobrenome").value,
         form.elements.namedItem("email").value,
         form.elements.namedItem("cpf").value,
         form.elements.namedItem("telefone").value,
-        form.elements.namedItem("contato").value);
+        form.elements.namedItem("contato").value
+    );
+    console.log("Dados enviados:", data);
 
+    alert(`Obrigado, ${data.nome}! Seus dados foram enviados com sucesso.`);
 }
 
 function Enviar() {
+    var nome = document.querySelector("input[name='nome']");
 
-    var nome = document.getElementById("nomeid");
-
-    if (nome.value != "") {
-        alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
+    if (nome && nome.value.trim() !== "") {
+        alert('Obrigado, ' + nome.value + '! Seus dados foram encaminhados com sucesso.');
+    } else {
+        alert('Por favor, preencha o nome.');
     }
-
 }
